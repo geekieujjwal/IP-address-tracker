f13d0d3622b36e51fbb8c4dbb0fb9bfe68391a4f
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-08-24 14:33:52 +0530

    changed: details(HTML) | fixed:width of div(CSS) & added functionality of searching in js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const input = document.getElementById("search-inp");
-console.log(input.value);
 const ipAddress = document.getElementById("ipAddress");
 const locationID = document.getElementById("locationID");
 const timezone = document.getElementById("timezone");
@@ -7,39 +6,17 @@ const isp = document.getElementById("isp");
 
 const submitBtn = document.getElementById("submit-btn");
 
-submitBtn.addEventListener("click", () => {
-  fetchData();
-});
-
-input.addEventListener("keypress", (event) => {
-  if (event.keyCode === 13) {
-    submitBtn.click();
-  }
-});
-
-async function fetchData() {
-  const res = await fetch(
-    `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_zKOUvpjkDepQYUqRTCwoJd3lKmPXM&ipAddress=${input.value}`
-  );
-  const data = await res.json();
-  console.log(data);
-  ipAddress.textContent = input.value;
-  locationID.textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
-  timezone.textContent = data.location.timezone;
-  isp.textContent = data.isp;
-}
-
-let map;
+let map, userMarker;
 
 document.addEventListener("DOMContentLoaded", () => {
-  map = L.map("map").setView([0, 0], 2);
+  map = L.map("map").setView([0, 0], 10);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
   }).addTo(map);
 
   // Add a marker for the user's location
-  let userMarker = L.marker([0, 0]).addTo(map);
+  userMarker = L.marker([0, 0]).addTo(map);
 
   // Locate the user's current position
   map.locate({ setView: true, maxZoom: 16 });
@@ -57,3 +34,34 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("Could not retrieve your location.");
   });
 });
+
+//On click of submit button
+submitBtn.addEventListener("click", () => {
+  fetchData();
+});
+
+//On click of Enter on input
+input.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    submitBtn.click();
+  }
+});
+
+//Fetching
+async function fetchData() {
+  const res = await fetch(
+    `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_zKOUvpjkDepQYUqRTCwoJd3lKmPXM&ipAddress=${input.value}`
+  );
+  const data = await res.json();
+  ipAddress.textContent = input.value;
+  locationID.textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+  timezone.textContent = data.location.timezone;
+  isp.textContent = data.isp;
+
+  let latlng = L.latLng(data.location.lng, data.location.lng);
+
+  map.setView([data.location.lat, data.location.lng], 16);
+
+  userMarker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+  userMarker.bindPopup("You searched for this position");
+}
